65113774b8b3e789bdafb9825a7e0120505d4318
Ashutosh <mishrashutosh1998@gmail.com>
2022-11-10 13:31:11 +0530

    Heroku procfile added

--- src/route-urls.js
+++ src/route-urls.js
@@ -1,2 +1,8 @@
-export const allBlogs = "http://127.0.0.1:8000/blogs";
-export const blogCrud = "http://127.0.0.1:8000/blog";
+// for connection to heroku backend app
+export const allBlogs = "https://personal-blog-backend-fastapi.herokuapp.com/blogs"; 
+export const blogCrud = "https://personal-blog-backend-fastapi.herokuapp.com/blog";
+
+
+//for local host connect
+// export const allBlogs = "http://127.0.0.1:8000/blogs";  
+// export const blogCrud = "http://127.0.0.1:8000/blog";
